08a70d32403470b14576927295343ecb27021059
muyoo <44095535+muyoou@users.noreply.github.com>
2025-01-29 21:51:50 +0800

    feat: 更改提示词结构，优化了初始化表格困难的问题

--- index.js
+++ index.js
@@ -13,22 +13,20 @@ const defaultSettings = {
     message_template: `# dataTable表格
 dataTable是一个用于储存故事数据的csv格式表格，可以作为你推演下文的重要参考。推演的下文可以在表格基础上做出发展，并影响表格。
 ## A. 表格说明及数据
-你可以在这里查看所有的表格数据，以及表格的说明。表格中表名格式为[tableIndex:表名]例如[2:角色特征表格];列名的格式为[colIndex:列名]例如[2:示例列];行名的格式为[rowIndex]。
+你可以在这里查看所有的表格数据，以及表格的说明和修改表格的触发条件。表格中表名格式为[tableIndex:表名]例如[2:角色特征表格];列名的格式为[colIndex:列名]例如[2:示例列];行名的格式为[rowIndex]。
 {{tableData}}
-# 增删改dataTable规则
-当你生成正文后，根据下文的规则，如果判断数据dataTable中的内容需要增删改，则使用后文中的操作方法进行。
+# 增删改dataTable操作方法
+当你生成正文后，根据前面所列的增删改触发条件，如果判断数据dataTable中的内容需要增删改，则使用这里的操作方法进行。
 注意：
 1. 当用户要求修改表格时，用户要求的优先级最高。
-2. 当所有表格都为空时，应立即积极的根据上下文填充表格。
-3. 插入行时，应上帝视角填写所有列，禁止写成未知或者空值。
+3. 使用insertRow函数插入行时，应上帝视角填写所有列，禁止写成未知或者空值。
 4. 单元格中，不要出现逗号，语义分割应使用/代替。
-5. 当表格中出现undefined、暂无时，应立即填充该单元格。
-{{tableEditRules}}
-# 增删改dataTable操作方法
-你可以使用以下函数对表格的数据进行增删改操作。
+5. 当表格中出现undefined、暂无时，应立即更新该单元格。
+
 ## 1. 在某个表格中插入新行，使用insertRow函数：
 insertRow(tableIndex:number, data:{[colIndex:number]:string|number})
 例如：insertRow(0, {0: '2021-10-01', 1: '12:00', 2: '教室', 3: '悠悠'})
+注意：请检查data:{[colIndex:number]:string|number}参数是否包含所有的colIndex，且禁止填写为未知。
 ## 2. 在某个表格中删除行，使用deleteRow函数：
 deleteRow(tableIndex:number, rowIndex:number)
 例如：deleteRow(0, 0)
@@ -36,45 +34,55 @@ deleteRow(tableIndex:number, rowIndex:number)
 updateRow(tableIndex:number, rowIndex:number, data:{[colIndex:number]:string|number})
 例如：updateRow(0, 0, {3: '惠惠'})
 
-你可以进行多次操作，然后将这些操作代码放入<tableEdit>标签中输出。
+你需要在<tableEdit>标签中输出对每个表格的检视过程，使用js注释写简短的判断依据。如果需要增删改，则使用js的函数写法调用函数。
 输出示例：
 <tableEdit>
 <!--
-insertRow(0, {0: '2021-10-01', 1: '12:00', 2: '教室', 3: '惠惠/悠悠'})
-insertRow(4, {0: '惠惠', 1: '惠惠向悠悠表白', 2: '2021-10-01', 3: '教室',4:'感动'})
-updateRow(2, 0, {3: '喜欢'})
+// 时空表格中的角色发生了改变，需要更新
+updateRow(0, 0, {3: '惠惠/悠悠'})
+// 角色特征表格由于出现了新人物，需要插入
+insertRow(1, {1:'悠悠', 1:'身高170/体重60kg/身材娇小/黑色长发', 2:'开朗活泼', 3:'学生', 4:'打羽毛球, 5:'鬼灭之刃', 6:'宿舍', 7:'是运动部部长'})
+// 角色与<user>社交表格由于出现了新人物，需要插入
+insertRow(2, {1:'悠悠', 1:'喜欢', 2:'依赖/喜欢', 3:'高'})
+// 任务、命令或者约定表格由于没有新任务、命令或者约定，所以不需要操作
+// 重要事件历史表格由于悠悠向惠惠表白，所以需要插入
+insertRow(4, {0: '惠惠/悠悠', 1: '惠惠向悠悠表白', 2: '2021-10-01', 3: '教室',4:'感动'})
+// 重要物品表格由于没有新物品，所以不需要操作
 -->
 </tableEdit>
-`
+`,
+    tableStructure: [
+        {
+            tableName: "时空表格", tableIndex: 0, columns: ['日期', '时间', '地点（当前描写）', '此地角色'], columnsIndex: [0, 1, 2, 3], enable: true, Required: true, note: "记录时空信息的表格，应保持在一行",
+            initNode: '本轮需要记录当前时间、地点、人物信息，使用insertRow函数', updateNode: "当描写的场景，时间，人物变更时", deleteNode: "此表大于一行时应删除多余行"
+        },
+        {
+            tableName: '角色特征表格', tableIndex: 1, columns: ['角色名', '身体特征', '性格', '职业', '爱好', '喜欢的事物（作品、虚拟人物、物品等）', '住所', '其他重要信息'], enable: true, Required: true, columnsIndex: [0, 1, 2, 3, 4, 5, 6, 7], note: '角色天生或不易改变的特征csv表格，思考本轮有否有其中的角色，他应作出什么反应',
+            initNode: '本轮必须从上文寻找已知的所有角色使用insertRow插入，角色名不能为空', insertNode: '当本轮出现表中没有的新角色时，应插入', updateNode: "当角色的身体出现持久性变化时，例如伤痕/当角色有新的爱好，职业，喜欢的事物时/当角色更换住所时/当角色提到重要信息时", deleteNode: ""
+        },
+        {
+            tableName: '角色与<user>社交表格', tableIndex: 2, columns: ['角色名', '对<user>关系', '对<user>态度', '对<user>好感'], columnsIndex: [0, 1, 2, 3], enable: true, Required: true, note: '思考如果有角色和<user>互动，应什么态度',
+            initNode: '本轮必须从上文寻找已知的所有角色使用insertRow插入，角色名不能为空', insertNode: '当本轮出现表中没有的新角色时，应插入', updateNode: "当角色和<user>的交互不再符合原有的记录时/当角色和<user>的关系改变时", deleteNode: ""
+        },
+        {
+            tableName: '任务、命令或者约定表格', tableIndex: 3, columns: ['角色', '任务', '地点', '持续时间'], columnsIndex: [0, 1, 2, 3], enable: true, Required: false, note: '思考本轮是否应该执行任务/赴约',
+            insertNode: '当特定时间约定一起去做某事时/某角色收到做某事的命令或任务时', updateNode: "", deleteNode: "当大家赴约时/任务或命令完成时/任务，命令或约定被取消时"
+        },
+        {
+            tableName: '重要事件历史表格', tableIndex: 4, columns: ['角色', '事件简述', '日期', '地点', '情绪'], columnsIndex: [0, 1, 2, 3, 4], enable: true, Required: false, note: '',
+            insertNode: '当某个角色经历让自己印象深刻的事件时，比如表白、分手等', updateNode: "", deleteNode: ""
+        },
+        {
+            tableName: '重要物品表格', tableIndex: 5, columns: ['拥有人', '物品描述', '物品名', '重要原因'], columnsIndex: [0, 1, 2, 3], enable: true, Required: false, note: '对某人很贵重或有特殊纪念意义的物品',
+            insertNode: '当某人获得了贵重或有特殊意义的物品时/当某个已有物品有了特殊意义时', updateNode: "", deleteNode: ""
+        },
+    ]
 };
 
-// 默认的表格结构设置
-const defaultTableData = [
-    {
-        tableName: "时空表格", tableIndex: 0, columns: ['日期', '时间', '地点（当前描写）', '此地角色'], columnsIndex: [0, 1, 2, 3], note: "记录时空信息的表格，应保持在一行",
-        isertNode: '当此表格0行时应当插入一行', updateNode: "当描写的场景，时间，人物变更时", deleteNode: "此表大于一行时应删除多余行"
-    },
-    {
-        tableName: '角色特征表格', tableIndex: 1, columns: ['角色名', '身体特征', '性格', '职业', '爱好', '喜欢的事物（作品、虚拟人物、物品等）', '住所', '其他重要信息'], note: '角色天生或不易改变的特征csv表格，思考本轮有否有其中的角色，他应作出什么反应',
-        isertNode: '当本轮出现表中没有的新角色时，应插入/当此表为空时，必须从上文寻找已知角色插入，角色名不能为空', updateNode: "当角色的身体出现持久性变化时，例如伤痕/当角色有新的爱好，职业，喜欢的事物时/当角色更换住所时/当角色提到重要信息时", deleteNode: ""
-    },
-    {
-        tableName: '角色与<user>社交表格', tableIndex: 2, columns: ['角色名', '对<user>关系', '对<user>态度', '对<user>好感'], note: '思考如果有角色和<user>互动，应什么态度',
-        isertNode: '当本轮出现表中没有的新角色时，应插入/当此表为空时，必须从上文寻找已知角色插入，角色名不能为空', updateNode: "当角色和<user>的交互不再符合原有的记录时/当角色和<user>的关系改变时", deleteNode: ""
-    },
-    {
-        tableName: '任务、命令或者约定表格', tableIndex: 3, columns: ['角色', '任务', '地点', '持续时间'], note: '思考本轮是否应该执行任务/赴约',
-        isertNode: '当特定时间约定一起去做某事时/某角色收到做某事的命令或任务时', updateNode: "", deleteNode: "当大家赴约时/任务或命令完成时/任务，命令或约定被取消时"
-    },
-    {
-        tableName: '重要事件历史表格', tableIndex: 4, columns: ['角色', '事件简述', '日期', '地点', '情绪'], note: '',
-        isertNode: '当某个角色经历让自己印象深刻的事件时，比如表白、分手等', updateNode: "", deleteNode: ""
-    },
-    {
-        tableName: '重要物品表格', tableIndex: 5, columns: ['拥有人', '物品描述', '物品名', '重要原因'], note: '对某人很贵重或有特殊纪念意义的物品',
-        isertNode: '当某人获得了贵重或有特殊意义的物品时/当某个已有物品有了特殊意义时', updateNode: "", deleteNode: ""
-    },
-]
+
+function findTableStructureByIndex(index) {
+    return extension_settings.muyoo_dataTable.tableStructure.find(table => table.tableIndex === index);
+}
 
 function loadSettings() {
     extension_settings.muyoo_dataTable = extension_settings.muyoo_dataTable || {};
@@ -83,20 +91,21 @@ function loadSettings() {
             extension_settings.muyoo_dataTable[key] = defaultSettings[key];
         }
     }
+    extension_settings.muyoo_dataTable.message_template = defaultSettings.message_template
     $(`#dataTable_injection_mode option[value="${extension_settings.muyoo_dataTable.injection_mode}"]`).attr('selected', true);
     $('#dataTable_deep').val(extension_settings.muyoo_dataTable.deep);
     $('#dataTable_message_template').val(extension_settings.muyoo_dataTable.message_template);
 }
 
 function resetSettings() {
-    extension_settings.muyoo_dataTable = {...defaultSettings};
+    extension_settings.muyoo_dataTable = { ...defaultSettings };
     loadSettings();
     saveSettingsDebounced();
     toastr.success('已重置设置');
 }
 
 function initAllTable() {
-    return defaultTableData.map(data => new Table(data.tableName, data.tableIndex, data.columns, data.note))
+    return extension_settings.muyoo_dataTable.tableStructure.map(data => new Table(data.tableName, data.tableIndex, data.columns, data.note))
 }
 
 function checkPrototype(dataTable) {
@@ -159,7 +168,7 @@ export function initTableData() {
 
 function getAllPrompt(tables) {
     const tableDataPrompt = tables.map(table => table.getTableText()).join('\n')
-    return extension_settings.muyoo_dataTable.message_template.replace('{{tableData}}', tableDataPrompt).replace('{{tableEditRules}}', getTableEditRules())
+    return extension_settings.muyoo_dataTable.message_template.replace('{{tableData}}', tableDataPrompt)
 }
 
 function copyTableList(tableList) {
@@ -175,6 +184,21 @@ function handleCellValue(cell) {
     return ''
 }
 
+function getEmptyTablePrompt(Required, node) {
+    return '（此表格为空' + (Required ? (node ? ('，' + node) : '') : '') + '）\n'
+}
+
+function getTableEditRules(structure, isEmpty) {
+    if (structure.Required && isEmpty) return '【增删改触发条件】\n插入：' + structure.initNode + '\n'
+    else {
+        let editRules = '【增删改触发条件】\n'
+        if (structure.insertNode) editRules += ('插入：' + structure.insertNode + '\n')
+        if (structure.updateNode) editRules += ('更新：' + structure.updateNode + '\n')
+        if (structure.deleteNode) editRules += ('删除：' + structure.deleteNode + '\n')
+        return editRules
+    }
+}
+
 class Table {
     constructor(tableName, tableIndex, columns, note = '', content = []) {
         this.tableName = tableName
@@ -185,11 +209,13 @@ class Table {
     }
 
     getTableText() {
+        const structure = findTableStructureByIndex(this.tableIndex)
+        if (!structure) return
         const title = `* ${this.tableIndex}:${this.tableName}\n`
-        const node = this.note && this.note !== '' ? '说明：' + this.note + '\n' : ''
+        const node = this.note && this.note !== '' ? '【说明】' + this.note + '\n' : ''
         const headers = "rowIndex," + this.columns.map((colName, index) => index + ':' + colName).join(',') + '\n'
-        const rows = this.content.length > 0 ? this.content.map((row, index) => index + ',' + row.join(',')).join('\n') + '\n' : ''
-        return title + node + headers + rows
+        const rows = this.content.length > 0 ? (this.content.map((row, index) => index + ',' + row.join(',')).join('\n') + '\n') : getEmptyTablePrompt(structure.Required, structure.initNode)
+        return title + node + '【表格内容】\n' + headers + rows + getTableEditRules(structure, this.content.length == 0) + '\n'
     }
 
     insert(data) {
@@ -248,14 +274,6 @@ class Table {
 
 async function onChatChanged() {
     const chatMetadata = getContext().chatMetadata;
-}
-
-const getTableEditRules = () => {
-
-    const insetRule = "## A. 新增规则\n" + defaultTableData.map(table => table.isertNode === '' ? null : `[${table.tableIndex}:${table.tableName}]：${table.isertNode}\n`).filter(Boolean).join('')
-    const updateRule = "## B. 更新规则\n" + defaultTableData.map(table => table.updateNode === '' ? null : `[${table.tableIndex}:${table.tableName}]：${table.updateNode}\n`).filter(Boolean).join('')
-    const deleteRule = "## C. 删除规则\n" + defaultTableData.map(table => table.deleteNode === '' ? null : `[${table.tableIndex}:${table.tableName}]：${table.deleteNode}\n`).filter(Boolean).join('')
-    return insetRule + updateRule + deleteRule
 }
 
 let waitingTable = null
@@ -323,9 +341,9 @@ function executeTableEditFunction(functionList) {
 }
 
 function fixFunctionNameError(str) {
-    if (str.startsWith("update(")) 
+    if (str.startsWith("update("))
         return str.replace("update(", "updateRow(");
-    if (str.startsWith("insert(")) 
+    if (str.startsWith("insert("))
         return str.replace("insert(", "insertRow(");
     if (str.startsWith("delete("))
         return str.replace("delete(", "deleteRow(");
@@ -386,7 +404,7 @@ function getMesRole() {
 }
 
 async function onChatCompletionPromptReady(eventData) {
-    if (eventData.dryRun === true) return
+    //if (eventData.dryRun === true) return
     const promptContent = initTableData()
     eventData.chat.splice(extension_settings.muyoo_dataTable.deep, 0, { role: getMesRole(), content: promptContent })
     /* console.log("dryRun", eventData.dryRun)
@@ -401,7 +419,7 @@ async function onChatCompletionPromptReady(eventData) {
     if (false === eventData.dryRun) promptManager.render(false)
     const realIndex = getRealIndexInCollectionInDryRun('tableData', promptManager.messages.collection)
     eventData.chat.splice(realIndex, 0, { role: "system", content: promptContent }) */
-    console.log("最终提示", eventData.chat)
+    console.log("最终提示", promptContent)
 }
 
 function trimString(str) {
