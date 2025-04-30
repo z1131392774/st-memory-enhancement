bfdd13402b6f39cbb2c22c74efc4112b47feb02a
muyoo <44095535+muyoou@users.noreply.github.com>
2025-05-01 00:32:11 +0800

    feat: 切换版本

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ import {executeTranslation} from "./services/translate.js";
 
 console.log("______________________记忆插件：开始加载______________________")
 
-const VERSION = '2.0.0_dev'
+const VERSION = '2.0.0'
 
 const editErrorInfo = {
     forgotCommentTag: false,
