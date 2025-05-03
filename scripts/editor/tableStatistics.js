36ab33f08d67df453fc3191c504bdf84d10d6da8
tthis-x <tthis@qq.com>
2025-05-03 21:39:44 +0800

    feat: 优化储存数据结构，新增清理历史冗余单元格功能

--- scripts/editor/tableStatistics.js
+++ scripts/editor/tableStatistics.js
@@ -1,7 +1,5 @@
 import {BASE, DERIVED, EDITOR, SYSTEM, USER} from '../../core/manager.js';
 import {estimateTokenCount} from "../settings/standaloneAPI.js";
-// import { deleteRow, insertRow, updateRow } from "../oldTableActions.js";
-// import JSON5 from '../../utils/json5.min.mjs'
 
 const statistics = `
 <style>
@@ -25,6 +23,12 @@ const statistics = `
 <div class="table-statistics">
     <div id="dialogue_popup_text">
         <h3>表格数据统计</h3>
+        <div class="table-statistics-header">
+            <div class="menu_button_icon menu_button interactable gap5" id="clear_table_statistics_button" tabindex="0">
+                <i class="fa-solid fa-broom"></i>
+                <span>清理不在对话树的历史单元</a>
+            </div>
+        </div>
         <div class="table-statistics-content">
             <!-- 动态内容将在这里插入 -->
         </div>
@@ -32,7 +36,7 @@ const statistics = `
 </div>
 `
 
-function updataTableStatisticsData(container) {
+async function updataTableStatisticsData(container) {
     const { piece, deep } = BASE.getLastSheetsPiece();
     const sheetsData = BASE.sheetsData.context;
     if (!piece || !piece.hash_sheets) return;
@@ -67,6 +71,52 @@ function updataTableStatisticsData(container) {
     });
 }
 
+async function clearTableStatisticsButton(statisticsContainer) {
+    const chat = USER.getContext().chat;
+    if (!chat || chat.length === 0) return;
+
+    const messageHashSheets = JSON.parse(JSON.stringify(USER.getContext().chat
+        .filter(message => message.hash_sheets)
+        .map(message => message.hash_sheets)));
+    let hashList = []
+    messageHashSheets.forEach(hashSheets => {
+        Object.entries(hashSheets).forEach(([sheetId, sheet]) => {
+            hashList = [...hashList, ...sheet.map(row => row.flat()).flat()]
+        })
+    });
+    const filterDuplicateMap = new Set(hashList);
+
+    const sheetsData = BASE.sheetsData.context;
+    const cellHistories = sheetsData.map(sheet => sheet.cellHistory);
+    let cellHistoryHashNum = 0;
+    let lastCellHistoryHashNum = 0;
+    cellHistories.forEach((cellHistory, index) => {
+        cellHistory.forEach((cell, cellIndex) => {
+            lastCellHistoryHashNum++
+            if (cell && cell.uid && filterDuplicateMap.has(cell.uid)) {
+                cellHistoryHashNum++;
+                delete cell.CellAction;
+                delete cell.CellType;
+                delete cell.bridge;
+            } else {
+                cellHistories[index].splice(cellIndex, 1);
+            }
+        })
+    })
+
+    setTimeout(() => {
+        if (lastCellHistoryHashNum === cellHistoryHashNum) {
+            updataTableStatisticsData(statisticsContainer);
+            EDITOR.success(`清理历史单元格操作完成, 有效单元格数量: ${cellHistoryHashNum}`);
+            USER.saveChat()
+            return;
+        } else {
+            EDITOR.info(`本轮清理单元格数量: ${lastCellHistoryHashNum - cellHistoryHashNum}`);
+            clearTableStatisticsButton(statisticsContainer)
+        }
+    }, 0)
+}
+
 /**
  * 打开表格编辑历史记录弹窗
  * */
@@ -74,6 +124,11 @@ export async function openTableStatisticsPopup(){
     const manager = statistics;
     const tableStatisticsPopup = new EDITOR.Popup(manager, EDITOR.POPUP_TYPE.TEXT, '', { wide: true, allowVerticalScrolling: true });
     const statisticsContainer = $(tableStatisticsPopup.dlg)[0];
+    // 绑定清理按钮事件
+    const clearButton = $(statisticsContainer).find('#clear_table_statistics_button');
+    clearButton.on('click', () => {
+        clearTableStatisticsButton(statisticsContainer)
+    });
 
     updataTableStatisticsData(statisticsContainer);
 
